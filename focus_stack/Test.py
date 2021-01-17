1b324dafa65dbafc96052c0440c46c7bf255ad79
noah <noah.ed.peeters@gmail.com>
2021-01-17 19:14:35 +0100

    further tests
    Former-commit-id: f2ba3a9e8fa35919a2690087961a89d91539c352 [formerly b2e052545914f987431453f203542ed90cafa30d]
    Former-commit-id: fe2d76ea0d44d44dc262fcc19102d5dc99b2b168

--- focus_stack/Test.py
+++ focus_stack/Test.py
@@ -1,27 +1,24 @@
-import os
-from skimage import data, io
 from dask_image.imread import imread
-import dask_image.ndfilters
-import dask_image.ndmeasure
-import dask.array as da
-import matplotlib.pyplot as plt
+from dask_image import ndfilters
 import napari
+import cv2
 
 # Function to convert images to grayscale
 def grayscale(rgb):
     # Calculate luminance
     return ((rgb[..., 0] * 0.2125) + (rgb[..., 1] * 0.7154) + (rgb[..., 2] * 0.0721))
 
+
 # get images and load them with dask
-filename_pattern = os.path.join("images", "*.jpg")
-images = imread(filename_pattern)
+# filename_pattern = os.path.join("images", "*.jpg")
 
+images = imread("images/DSC_0374.jpg")
 
-# * Focus stack images
 grayscaled_images = grayscale(images)   # Convert images to grayscale
-
-#dask_image.ndfilters.gaussian_filter(images, 0, 0, "reflect", 0.0, 4.0) # Apply gaussian blur to images
+print(images.shape)
+print(grayscaled_images.shape)
+smoothed = ndfilters.gaussian_filter(grayscaled_images, sigma=[1, 1])
 
 with napari.gui_qt():
     viewer = napari.view_image(images)
-    viewer.add_image(grayscaled_images)
+    viewer.add_image(smoothed)
