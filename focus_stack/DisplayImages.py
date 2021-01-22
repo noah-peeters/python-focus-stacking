648ac58dde42949a7b0c4b4783c3a116dd54ef42
noah <noah.ed.peeters@gmail.com>
2021-01-22 21:10:42 +0100

    fix algorithm

--- focus_stack/DisplayImages.py
+++ focus_stack/DisplayImages.py
@@ -2,10 +2,21 @@ import napari
 from napari.qt.threading import thread_worker
 import numpy
 import glob
-import os, os.path
+from PIL import Image
 
 
-IMAGE_DIR = "test/*.jpg"
+IMAGE_DIR = "images/*.jpg"
+
+imageHeight = None
+imageWidth = None
+
+imgFileList = glob.glob(IMAGE_DIR)
+for index, imPath in enumerate(imgFileList):
+    if index == 0:
+        print("read img")
+        image = Image.open(imPath)
+        imageWidth, imageHeight = image.size
+
 
 
 with napari.gui_qt():
@@ -14,10 +25,6 @@ with napari.gui_qt():
     def stackedImghandler(imgArray):
         viewer.add_image(imgArray, name="Stacked Image", rgb=True)
         print("Stacked image has been loaded.")
-
-    def maskImghandler(imgArray):
-        viewer.add_image(imgArray, name="Mask")
-        print("Mask has been loaded.")
 
     def origImagesHandler(imgArray):
         viewer.add_image(imgArray, name="Original Images")
@@ -29,11 +36,7 @@ with napari.gui_qt():
 
     @thread_worker(connect={"returned": stackedImghandler})
     def loadStackedImage():
-        return numpy.memmap("stacked.img", mode="r", shape=(4000, 6000, 3))
-    
-    @thread_worker(connect={"returned": maskImghandler})
-    def loadMask():
-        return numpy.memmap("mask.img", mode="r", shape=(54, 4000, 6000)) # First dim is number of images in stack
+        return numpy.memmap("stacked.img", mode="r", shape=(imageHeight, imageWidth, 3))
 
     @thread_worker(connect={"returned": origImagesHandler})
     def loadImages():
@@ -41,7 +44,7 @@ with napari.gui_qt():
 
         imgFileList = glob.glob(IMAGE_DIR)
         for imPath in sorted(imgFileList):
-            images.append(numpy.memmap(imPath + ".img", mode="r", shape=(4000, 6000, 3)))
+            images.append(numpy.memmap(imPath + ".img", mode="r", shape=(imageHeight, imageWidth, 3)))
 
         return numpy.asarray(images)
 
@@ -51,12 +54,11 @@ with napari.gui_qt():
 
         imgFileList = glob.glob(IMAGE_DIR)
         for imPath in sorted(imgFileList):
-            grayscales.append(numpy.memmap(imPath + ".grayscale", mode="r", shape=(4000, 6000)))
+            grayscales.append(numpy.memmap(imPath + ".grayscale", mode="r", shape=(imageHeight, imageWidth)))
 
         return numpy.asarray(grayscales)
 
     # Run threaded functions
     loadStackedImage()
-    loadMask()
     loadImages()
     loadGrayscaleImages()
