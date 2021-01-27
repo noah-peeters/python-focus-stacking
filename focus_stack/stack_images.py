cd488d95f4ab0ae9e684a40f68b6f6344cc4a5ca
noah <noah.ed.peeters@gmail.com>
2021-01-27 15:11:06 +0100

    Test align algorithm (commented out)

--- focus_stack/stack_images.py
+++ focus_stack/stack_images.py
@@ -42,11 +42,17 @@ class FocusStacker(object):
         logger.info("concurrent processing images")
         processes = []
         image_objects = []
-        for img_fn in image_files:
-            _image = IndividualImage(img_fn, self._gaussian_blur_kernel_size, self._laplacian_kernel_size)
+        for i, img_fn in enumerate(image_files):
+            if i == 0:
+                # Don't align first image
+                _image = IndividualImage(img_fn, None, self._gaussian_blur_kernel_size, self._laplacian_kernel_size)
+            else:
+                # Align next images to first image (image_files[0])
+                _image = IndividualImage(img_fn, image_files[0], self._gaussian_blur_kernel_size, self._laplacian_kernel_size)
+
             processes.append(dask.delayed(_image.read_and_analyze)())
             image_objects.append(_image)
-        #    processes.append(dask.delayed(_image.read_and_analyze)(_image))
+
         # Load all images in parallel
         dask.compute(*processes)
 
@@ -83,37 +89,81 @@ class FocusStacker(object):
 
 class IndividualImage(object):
     def __init__(
-        self, image_filename, laplacian_kernel_size: int = 5, gaussian_blur_kernel_size: int = 5
-    ) -> None:
+        self, image_filename, first_image_path, laplacian_kernel_size: int = 5, gaussian_blur_kernel_size: int = 5) -> None:
         """Single Image Class, groups all processing
         """
         self._image_filename = image_filename
         self._laplacian_kernel_size = laplacian_kernel_size
         self._gaussian_blur_kernel_size = gaussian_blur_kernel_size
+        self._first_image_path = first_image_path
 
 
     def dimensions(self):
         return self._height, self._width
 
-    def _temp_filenames(self):
-        filename, file_extension = os.path.splitext(self._image_filename)
-        return filename + ".raw", filename+".grayscale.raw"
+    def _temp_filenames(self, imPath):
+        filename, file_extension = os.path.splitext(imPath)
+        return filename + ".raw", filename + ".grayscale.raw"
 
     def read_and_analyze(self):
-        _raw_fn, _grayscale_fn = self._temp_filenames()
+        _raw_fn, _grayscale_fn = self._temp_filenames(self._image_filename)
         # step 1: read image / prepare memmap 
         image = cv2.imread(self._image_filename)
         self._height, self._width, channels = image.shape
         memMappedImg = numpy.memmap(_raw_fn, mode="w+", shape=(self._height, self._width, 3)) # Create a memory mapped array for storing raw image data (matching image dimensions)
         memMappedImg[:] = numpy.asarray(image)
-        del memMappedImg 
 
-        # step 2: convert to grayscale
+        # step 2: align image to first one
+        # if self._first_image_path != None:
+        #     detector = cv2.ORB_create(100000)
+
+        #     def _find_homography(_img1_key_points: numpy.ndarray, _image_2_kp: numpy.ndarray, _matches: List):
+        #         image_1_points = numpy.zeros((len(_matches), 1, 2), dtype=numpy.float32)
+        #         image_2_points = numpy.zeros((len(_matches), 1, 2), dtype=numpy.float32)
+
+        #         for j in range(0, len(_matches)):
+        #             image_1_points[j] = _img1_key_points[_matches[j].queryIdx].pt
+        #             image_2_points[j] = _image_2_kp[_matches[j].trainIdx].pt
+
+        #         homography, mask = cv2.findHomography(
+        #             image_1_points, image_2_points, cv2.RANSAC, ransacReprojThreshold=2.0
+        #         )
+
+        #         return homography
+
+        #     first_image = numpy.memmap(_raw_fn, mode="r", shape=(self._height, self._width))
+        #     img_i_key_points, image_i_desc = detector.detectAndCompute(memMappedImg, None)
+        #     img1_key_points, image1_desc = detector.detectAndCompute(first_image, None)
+
+        #     bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+        #     raw_matches = bf.match(image_i_desc, image1_desc)
+
+        #     sort_matches = sorted(raw_matches, key=lambda x: x.distance)
+        #     matches = sort_matches[0:128]
+
+        #     homography_matrix = _find_homography(
+        #         img_i_key_points, img1_key_points, matches
+        #     )
+        #     aligned_img = cv2.warpPerspective(
+        #         memMappedImg,
+        #         homography_matrix,
+        #         (memMappedImg.shape[1], memMappedImg.shape[0]),
+        #         flags=cv2.INTER_LINEAR,
+        #     )
+            
+        #     memMappedImg[:] = numpy.asarray(aligned_img)
+        #     image = numpy.asarray(aligned_img)
+
+        # del memMappedImg
+
+        # step 3: convert to grayscale
         grayscale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         memMappedGrayscale = numpy.memmap(_grayscale_fn, mode="w+", shape=(self._height, self._width)) # Create a memory mapped array for storing raw image data (matching image dimensions)
-        memMappedGrayscale[:] = numpy.asarray(grayscale) 
+        memMappedGrayscale[:] = numpy.asarray(grayscale)
 
-        # step 3: on grayscale: Gaussian Blur + Laplacian
+        
+        # step 4: on aligned grayscale: Gaussian Blur + Laplacian
+        memMappedGrayscale = numpy.memmap(_grayscale_fn, mode="r+", shape=(self._height, self._width))
         blurredImg = cv2.GaussianBlur(memMappedGrayscale, (self._gaussian_blur_kernel_size, self._gaussian_blur_kernel_size), 0)
         laplacianGradient = cv2.Laplacian(blurredImg, -1, ksize=self._laplacian_kernel_size) # ddepth -1 for same as src image (cv2.CV_64F)
         memMappedGrayscale[:] = laplacianGradient   # Store processed (grayscale) image
@@ -121,12 +171,12 @@ class IndividualImage(object):
 
     
     def read_temp_files_as_mmap(self):
-        _raw_fn, _grayscale_fn = self._temp_filenames()
+        _raw_fn, _grayscale_fn = self._temp_filenames(self._image_filename)
         _img_raw = numpy.memmap(_raw_fn, mode="r", shape=(self._height, self._width, 3))
         _grayscale_raw = numpy.memmap(_grayscale_fn, mode="r", shape=(self._height, self._width))
         return _img_raw, _grayscale_raw 
 
     def cleanup_temp_files(self):
-        _raw_fn, _grayscale_fn = self._temp_filenames()
+        _raw_fn, _grayscale_fn = self._temp_filenames(self._image_filename)
         os.remove(_raw_fn)
         os.remove(_grayscale_fn)
