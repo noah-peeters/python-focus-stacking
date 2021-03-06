959abc518a02bd2a5c8b8d8cd6157f56751f43b9
Noah <noah.ed.peeters@gmail.com>
2021-03-06 23:31:42 +0100

    started ui preferences (themes) with saving

--- QtUi/main.py
+++ QtUi/main.py
@@ -63,8 +63,10 @@ class MainWindow(qtw.QMainWindow):
         self.setCentralWidget(self.main_layout)
         self.showMaximized()                    # Show fullscreen
 
+        self.Preferences = Preferences(self)    # Init Preferences
+
         from algorithm import MainAlgorithm
-        self.Algorithm = MainAlgorithm()        # Initialize algorithm
+        self.Algorithm = MainAlgorithm()        # Init algorithm
 
     # Setup file menu (topbar)
     def setup_file_menu(self):
@@ -81,29 +83,32 @@ class MainWindow(qtw.QMainWindow):
         file_menu = menu_bar.addMenu("&File")
         processing_menu = menu_bar.addMenu("&Processing")
         help_menu = menu_bar.addMenu("&Help")
+        edit_menu = menu_bar.addMenu("&Edit")
 
         # Create actions
         new_action = qtw.QAction("&New file", self, shortcut="Ctrl+N", triggered=self.create_new_file)
         open_action = qtw.QAction("&Open file", self, shortcut="Ctrl+O", triggered=self.open_file)
         save_file = qtw.QAction("&Save file", self, shortcut="Ctrl+S", triggered=self.save_file)
         load_images_action = qtw.QAction("&Load images", self, shortcut="Ctrl+L", triggered=self.load_images)
-        clear_loaded_images_action = qtw.QAction("&Clear loaded images", self, shortcut="Ctrl+Alt+C")
+        clear_loaded_images_action = qtw.QAction("&Clear loaded images", self, shortcut="Ctrl+Alt+C", triggered=self.clear_loaded_images)
         export_action = qtw.QAction("&Export", self, shortcut="Ctrl+E", triggered=self.export_image)
         quit_action = qtw.QAction("&Quit", self, shortcut="Ctrl+W", triggered=lambda: self.close())
-
         # Processing functions disabled by default (no images loaded)
         self.align_images_action = qtw.QAction("&Align images", self, shortcut="Ctrl+Shift+A", triggered=self.align_images, enabled=False)
         self.stack_images_action = qtw.QAction("&Stack Images", self, shortcut="Ctrl+Shift+S", triggered=self.stack_images, enabled=False)
         self.align_and_stack_images_action = qtw.QAction("&Align and stack images", self, shortcut="Ctrl+Shift+P", triggered=self.align_and_stack_images, enabled=False)
 
-        about_app_action = qtw.QAction("&About", self, triggered=self.about_application)
+        about_app_action = qtw.QAction("&About PyStacker", self, triggered=self.about_application)
         about_qt_action = qtw.QAction("&About Qt", self, triggered=qtw.qApp.aboutQt)
+
+        preferences_action = qtw.QAction("&Preferences", self, shortcut="Ctrl+P", triggered=self.open_preferences)
 
         # Setup help tips for actions
         new_action.setStatusTip("Create a new file. Unsaved progress will be lost!")
         open_action.setStatusTip("Open a file on disk. Unsaved progress will be lost!")
         save_file.setStatusTip("Save file to disk.")
         load_images_action.setStatusTip("Load images from disk.")
+        clear_loaded_images_action.setStatusTip("Clear all loaded images.")
         export_action.setStatusTip("Export output image.")
         quit_action.setStatusTip("Exit the application. Unsaved progress will be lost!")
 
@@ -114,11 +119,14 @@ class MainWindow(qtw.QMainWindow):
         about_app_action.setStatusTip("About this application.")
         about_qt_action.setStatusTip("About Qt, the framework that was used to design this ui.")
 
+        preferences_action.setStatusTip("Preferences: themes and other settings.")
+
         # Icons for actions
         new_action.setIcon(get_icon("SP_FileIcon"))
         save_file.setIcon(get_icon("SP_DialogSaveButton"))
         open_action.setIcon(get_icon("SP_DialogOpenButton"))
         load_images_action.setIcon(get_icon("SP_FileDialogNewFolder"))
+        clear_loaded_images_action.setIcon(get_icon("SP_DialogDiscardButton"))
         export_action.setIcon(get_icon("SP_DialogYesButton"))
         quit_action.setIcon(get_icon("SP_TitleBarCloseButton"))
 
@@ -126,11 +134,14 @@ class MainWindow(qtw.QMainWindow):
         self.stack_images_action.setIcon(get_icon("SP_TitleBarNormalButton"))
         self.align_and_stack_images_action.setIcon(get_icon("SP_BrowserReload"))
 
+        about_qt_action.setIcon(get_icon("SP_TitleBarMenuButton"))
+
         # Add actions to menu
         file_menu.addAction(new_action)
         file_menu.addAction(open_action)
         file_menu.addAction(save_file)
         file_menu.addAction(load_images_action)
+        file_menu.addAction(clear_loaded_images_action)
         file_menu.addAction(export_action)
         file_menu.addAction(quit_action)
 
@@ -140,6 +151,8 @@ class MainWindow(qtw.QMainWindow):
 
         help_menu.addAction(about_app_action)
         help_menu.addAction(about_qt_action)
+
+        edit_menu.addAction(preferences_action)
 
     def create_new_file(self):
         print("Create new file")
@@ -235,8 +248,14 @@ class MainWindow(qtw.QMainWindow):
         print("Align and stack images")
 
     def clear_loaded_images(self):
-        self.loaded_image_files = []
-        self.toggle_processing_actions(False)
+        # Display confirmation dialog
+        reply = qtw.QMessageBox.question(self, "Clear loaded images", "Are you sure you want to clear all loaded images?", qtw.QMessageBox.Yes|qtw.QMessageBox.No)
+        if reply == qtw.QMessageBox.Yes:
+            # Clear (user confirmed)
+            self.loaded_image_files = []
+            self.main_layout.set_image_list(self.loaded_image_files)
+            self.toggle_processing_actions(False)
+
 
     # Display result message after operation finished
     def result_message(self, returned_table, props):
@@ -281,7 +300,7 @@ class MainWindow(qtw.QMainWindow):
         qtw.QMessageBox.about(self, "About PyStacker",
         "<b>PyStacker</b> is an application written in <b>Python</b> and <b>Qt</b> that is completely open-source: "
         "<a href='https://github.com/noah-peeters/python-focus-stacking'>PyStacker on github</a>"
-        " and aims to provide a professional focus stacking application for free."
+        " and aims to provide an advanced focus stacking application for free."
         )
 
     # Enable or disable processing actions (only enable when images are loaded)
@@ -289,6 +308,9 @@ class MainWindow(qtw.QMainWindow):
         self.align_images_action.setEnabled(bool)
         self.stack_images_action.setEnabled(bool)
         self.align_and_stack_images_action.setEnabled(bool)
+
+    def open_preferences(self):
+        self.Preferences.exec_()
 
 # Main layout for MainWindow (splitter window)
 class MainLayout(qtw.QWidget):
@@ -322,23 +344,33 @@ class MainLayout(qtw.QWidget):
         for path in image_paths:
             self.image_names.append(Utilities.get_file_name(path))
 
-        self.list_widget.image_list.addItems(self.image_names)
+        if len(self.image_names) > 0:
+            self.list_widget.image_list.addItems(self.image_names)              # Add image names
+        else:
+            self.list_widget.image_list.addItems(self.list_widget.default_list) # Add default
 
     def update_image(self, item):
-        index = self.image_names.index(item.text())
+        # Try getting image index
+        try:
+            index = self.image_names.index(item.text())
+        except ValueError:
+            return
+
         image = self.image_paths[index]
-        self.image_widget.image.setPixmap(qtg.QPixmap(image))
+        if image:
+            self.image_widget.image.setPixmap(qtg.QPixmap(image))
 
 
 # Loaded images list
 class LoadedImagesWidget(qtw.QWidget):
+    default_list = ["Loaded images will appear here.", "Please load them in from the 'file' menu."]
     def __init__(self, parent):
         super().__init__(parent)
 
         self.image_list = qtw.QListWidget()
         self.image_list.setAlternatingRowColors(True)
         self.image_list.setSelectionMode(qtw.QAbstractItemView.ExtendedSelection)
-        self.image_list.addItems(["Loaded images will appear here."])
+        self.image_list.addItems(self.default_list)
 
         layout = qtw.QVBoxLayout()
         layout.addWidget(self.image_list)
@@ -358,31 +390,94 @@ class ImageWidget(qtw.QWidget):
         layout.addWidget(self.image)
         self.setLayout(layout)
 
+# preferences class
+class Preferences(qtw.QDialog):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        self.Application = parent
+
+        self.setWindowTitle("Preferences")
+        self.setWindowModality(qtc.Qt.ApplicationModal)
+        self.theme_setting = qtw.QComboBox()
+
+        apply_button = qtw.QPushButton()
+        apply_button.setText("Apply")
+        apply_button.clicked.connect(self.apply_settings)
+
+        layout = qtw.QGridLayout()
+        layout.addWidget(self.theme_setting, 0, 0, 1, 2)
+        layout.addWidget(apply_button, 1, 1)
+        self.setLayout(layout)
+
+        # Setup settings
+        self.settings = qtc.QSettings("PyStacker", "Preferences")
+        self.load_settings()
+
+    # Apply all settings
+    def apply_settings(self):
+        # Set theme
+        applied_theme = self.theme_setting.currentText()
+        self.set_color_theme(applied_theme)
+        ls = []
+        if applied_theme == "Dark theme":
+            ls = ["Dark theme", "Light theme"]
+        else:
+            ls = ["Light theme", "Dark theme"]
+        
+        self.settings.setValue("Theme", ls)
+
+        self.theme_setting.clear()
+        self.theme_setting.insertItems(0, ls)
+    
+    def set_color_theme(self, theme_name):
+        palette = qtg.QPalette()
+        if theme_name == "Dark theme":
+            dark_gray = qtg.QColor(53, 53, 53)
+            gray = qtg.QColor(128, 128, 128)
+            black = qtg.QColor(25, 25, 25)
+            blue = qtg.QColor(42, 130, 218)
+
+            palette.setColor(qtg.QPalette.Window, dark_gray)
+            palette.setColor(qtg.QPalette.WindowText, qtc.Qt.white)
+            palette.setColor(qtg.QPalette.Base, black)
+            palette.setColor(qtg.QPalette.AlternateBase, dark_gray)
+            palette.setColor(qtg.QPalette.ToolTipBase, blue)
+            palette.setColor(qtg.QPalette.ToolTipText, qtc.Qt.white)
+            palette.setColor(qtg.QPalette.Text, qtc.Qt.white)
+            palette.setColor(qtg.QPalette.Button, dark_gray)
+            palette.setColor(qtg.QPalette.ButtonText, qtc.Qt.white)
+            palette.setColor(qtg.QPalette.Link, blue)
+            palette.setColor(qtg.QPalette.Highlight, blue)
+            palette.setColor(qtg.QPalette.HighlightedText, black)
+
+            palette.setColor(qtg.QPalette.Active, qtg.QPalette.Button, gray.darker())
+            palette.setColor(qtg.QPalette.Disabled, qtg.QPalette.ButtonText, gray)
+            palette.setColor(qtg.QPalette.Disabled, qtg.QPalette.WindowText, gray)
+            palette.setColor(qtg.QPalette.Disabled, qtg.QPalette.Text, gray)
+            palette.setColor(qtg.QPalette.Disabled, qtg.QPalette.Light, dark_gray)
+            self.Application.setPalette(palette)
+        elif theme_name == "Light theme":
+            self.Application.setPalette(self.Application.style().standardPalette())
+
+    # Load saved settings
+    def load_settings(self):
+        # Load saved theme
+        theme = self.settings.value("Theme", ["Dark theme", "Light theme"])[0]
+        self.set_color_theme(theme)
+        ls = []
+        if theme == "Dark theme":
+            ls = ["Dark theme", "Light theme"]
+        else:
+            ls = ["Light theme", "Dark theme"]
+        self.theme_setting.clear()
+        self.theme_setting.insertItems(0, ls)
 
 if __name__ == "__main__":
     app = qtw.QApplication(sys.argv)
     window = MainWindow()
-
-    # Force the style to be the same on all OSs:
+    
+    # Fore fusion style
     app.setStyle("Fusion")
-
-    # Now use a palette to switch to dark colors:
-    palette = qtg.QPalette()
-    palette.setColor(qtg.QPalette.Window, qtg.QColor(53, 53, 53))
-    palette.setColor(qtg.QPalette.WindowText, qtc.Qt.white)
-    palette.setColor(qtg.QPalette.Base, qtg.QColor(15, 15, 15))
-    palette.setColor(qtg.QPalette.AlternateBase, qtg.QColor(53, 53, 53))
-    palette.setColor(qtg.QPalette.ToolTipBase, qtc.Qt.white)
-    palette.setColor(qtg.QPalette.ToolTipText, qtc.Qt.white)
-    palette.setColor(qtg.QPalette.Text, qtc.Qt.white)
-    palette.setColor(qtg.QPalette.Button, qtg.QColor(53, 53, 53))
-    palette.setColor(qtg.QPalette.ButtonText, qtc.Qt.white)
-    palette.setColor(qtg.QPalette.BrightText, qtc.Qt.red)
-    palette.setColor(qtg.QPalette.Highlight, qtg.QColor(122, 25, 177))
-    palette.setColor(qtg.QPalette.HighlightedText, qtc.Qt.black)
-    palette.setColor(qtg.QPalette.Link, qtg.QColor(42, 130, 218))
-    app.setPalette(palette)
-
-    app.setStyleSheet("QToolTip { color: #ffffff; background-color: #2a82da; border: 1px solid white; }")
 
     sys.exit(app.exec_())
