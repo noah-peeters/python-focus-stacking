58ea9a718b3517b464dcf46c7c4f234da5123bc4
Noah Peeters <noah.ed.peeters@gmail.com>
2021-03-15 20:35:13 +0100

    update linter

--- QtUi/tests/utilities_test.py
+++ QtUi/tests/utilities_test.py
@@ -1,5 +1,6 @@
 from ..utilities import Utilities
 import pytest
 
+
 def test_format_seconds():
-    assert Utilities().format_seconds(65432) == '18:10:32'
+    assert Utilities().format_seconds(65432) == "18:10:32"
